2da39b7b983e7b69bef978405233447b92cfe673
atriilmajunirahim <37724871+atriilmajunirahim@users.noreply.github.com>
2018-05-27 22:46:06 +0800

    Add files via upload

---
+++ perkalian.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Atri
+ */
+public class perkalian {
+     public static void main (String [] args){
+         int x = 8 , y ;
+     for (int i=1 ; i<=10;++i){
+         y = i * x ;
+          System.out.println ( i + " x " + x + " = " + y);
+     }
+     }
+
+    
+    
+}
